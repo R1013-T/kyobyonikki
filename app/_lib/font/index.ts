0ae6e1e7f1e2fa13169368523e5e729fad984983
R1013-T <nosuke1013@icloud.com>
2023-12-19 13:38:56 +0900

    create metadata and font

---
+++ app/_lib/font/index.ts
@@ -0,0 +1,26 @@
+import localFont from 'next/font/local'
+
+export const LINESeedJP = localFont({
+  src: [
+    {
+      path: './LINESeedJP_Th.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './LINESeedJP_Rg.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './LINESeedJP_Bd.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './LINESeedJP_Eb.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+})
