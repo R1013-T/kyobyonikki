cd957de74f7532c89f7273363c28df32c270e752
R1013-T <nosuke1013@icloud.com>
2023-12-27 21:21:07 +0900

    change header style

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -11,7 +11,9 @@ export default function MainLayout({
     <main className="h-dvh lg:flex overflow-hidden relative">
       <Nav />
       <HamburgerMenu />
-      <MainHeader />
+      <div className="lg:hidden">
+        <MainHeader />
+      </div>
       <article className="h-full w-full mt-10 px-3 lg:mt-0 pt-3 max-w-xl mx-auto lg:max-w-none">
         {children}
       </article>
